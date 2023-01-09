3abef968146e3cb7d3115fac308848ca2e246c30
lintangsutawika <lintang@sutawika.com>
2023-01-09 04:25:36 +0000

    latest update

--- analysis/term_frequency.py
+++ analysis/term_frequency.py
@@ -1,18 +1,31 @@
+import re
 import os
+
+import ast
 import json
 
+import pandas as pd
+
+import lm_eval.models
 from lm_eval import tasks, evaluator
 
+# device = "cuda"
+device = "0"
 model_list = [
-    "EleutherAI/pythia-19m-deduped",
-    "EleutherAI/pythia-125m-deduped",
-    "EleutherAI/pythia-350m-deduped",
+    ["EleutherAI/pythia-13b-deduped", 32],
+    ["EleutherAI/pythia-6.7b-deduped", 32],
+    ["EleutherAI/pythia-2.7b-deduped", 64],
+    ["EleutherAI/pythia-1.3b-deduped", 64],
+    ["EleutherAI/pythia-800m-deduped", 128],
+    ["EleutherAI/pythia-350m-deduped", 128],
+    ["EleutherAI/pythia-125m-deduped", 256],
+    ["EleutherAI/pythia-19m-deduped", 256],
     ]
 
 eval_steps, max_steps = 13_000, 143_000
 checkpoint_list = list(range(eval_steps, max_steps+eval_steps, eval_steps))
 
-few_shot_list = [0,2,4,8,16]
+few_shot_list = [16, 8, 4, 2, 0]
 
 task_names = [
     "numerical_reasoning_arithmetic_multiplication",
@@ -28,26 +41,71 @@ task_names = [
     "numerical_reasoning_convert_decade_year",
     ]
 
-device = "cpu"
 
-for model in model_list:
+def evaluate_num_reasoning(model_name, device, batch_size=64):
+
+    all_results_df = pd.DataFrame()
     for checkpoint in checkpoint_list:
-
         revision = "step{}".format(checkpoint)
-
+        model_args="pretrained={},revision={}".format(model_name, revision)
+        print("Building Model")
+        model = lm_eval.models.get_model("gptneo").create_from_arg_string(
+            model_args, {"batch_size": batch_size, "device": device}
+        )
         for n in few_shot_list:
+            print("processing {} at step: {} with batch size {} and {}-shots".format(model_name, checkpoint, batch_size, n))
 
             results = evaluator.simple_evaluate(
-                model="gpt2",
-                model_args="pretrained={},revision={}".format(model, revision),
+                model=model,
+                # model="gptneo",
+                # model_args="pretrained={},revision={}".format(model_name, revision),
                 tasks=task_names,
                 num_fewshot=n,
-                batch_size=1,
+                batch_size=batch_size,
                 device=device,
-                # no_cache=False,
+                no_cache=True,
             )
 
-            dumped = json.dumps(results, indent=2)
-            output_path = "output_{}_{}_{}-shot.csv".format(model, checkpoint, n)
-            with open(output_path, "w") as f:
-                f.write(dumped)
+            for task in task_names:
+                results_dict = {
+                    "model": model_name,
+                    "checkpoint": checkpoint,
+                    "task": task,
+                    "fewshot": n,
+                    **results['results'][task]
+                    }
+
+                all_results_df = pd.concat(
+                    [all_results_df, pd.Series(results_dict).to_frame().T],
+                    ignore_index=True
+                    )
+        
+        all_results_df.to_csv(
+            "lm_cache/{}_numerical_reasoning_through_time.csv".format(
+                re.sub("/", "_", model_name)),
+                index=False
+            )
+                    
+if __name__ == "__main__":
+
+    # from multiprocessing import Process
+
+    # for idx, (model_name, batch_size) in enumerate(model_list):
+    #     p = Process(target=evaluate_num_reasoning, args=(model_name, str(idx), batch_size,))
+    #     p.daemon = True
+    #     p.start()
+
+    # import argparse
+
+    # parser = argparse.ArgumentParser(description='Eval on Num Reasoning')
+    # parser.add_argument('--model_name')
+    # parser.add_argument('--device')
+    # parser.add_argument('--batch_size', type=int)
+    # args = parser.parse_args()
+
+    for idx, (model_name, batch_size) in enumerate(model_list):
+        evaluate_num_reasoning(
+            model_name,
+            "cuda",
+            batch_size
+            )
