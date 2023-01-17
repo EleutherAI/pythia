4b47ff2746d50adccd4e88d4beee703da0cacba0
lintangsutawika <lintang@sutawika.com>
2023-01-17 17:32:20 +0000

    undo do_sample

--- analysis/term_frequency/model.py
+++ analysis/term_frequency/model.py
@@ -150,7 +150,7 @@ class GPTNeoLM(BaseLM):
 
     def _model_generate(self, context, max_length, eos_token_id):
         return self.gpt2.generate(
-            **context, max_length=max_length, eos_token_id=eos_token_id, do_sample=True,
+            **context, max_length=max_length, eos_token_id=eos_token_id, # do_sample=True,
         )
 
     def greedy_until(self, requests):
