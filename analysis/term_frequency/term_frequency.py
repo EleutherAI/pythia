82731b228c58124f7d92222d4b5fe7dca18401da
lintangsutawika <lintang@sutawika.com>
2023-01-25 01:19:08 +0000

    for generating graphs

--- analysis/term_frequency/term_frequency.py
+++ analysis/term_frequency/term_frequency.py
@@ -1,57 +1,135 @@
+import re
+import os
+import sys
+import string
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+from tqdm import tqdm
 from collections import Counter
 
-few_shot_list = [16, 8, 4, 2, 0]
+# few_shot_list = [16, 8, 4, 2, 0]
+few_shot_list = [0, 2, 4, 8, 16]
 
 eval_steps, max_steps = 13_000, 143_000
 checkpoint_list = list(range(eval_steps, max_steps+eval_steps, eval_steps))
+show_checkpoints = list(range(eval_steps, max_steps+eval_steps, 2*eval_steps))
 
-task = "num_reasoning_arithmetic_multiplication"
 
-freq = Counter()
-for checkpoint in tqdm(checkpoint_list):
-    
+freq = {}
+_freq = Counter()
+for idx, checkpoint in enumerate(checkpoint_list):
+
     checkpoint_path = os.path.join("results", f"frequency_count_checkpoint_{checkpoint}.npy")
-    _dict =  np.load(checkpoint_path, allow_pickle=True)[()]
-    freq.update(_dict)
+    _dict = np.load(checkpoint_path, allow_pickle=True)[()]
+    _freq.update(_dict)
+    freq[checkpoint] = _freq.copy()
 
-model_size = ["13b", "6.7b", "2.7b", "1.3b", "800m", "350m", "125m", "19m"]
-x, y, z = [], [], []
-for size in model_size:
+task_names = [
+    "num_reasoning_arithmetic_multiplication",
+    "num_reasoning_arithmetic_addition",
+    "num_reasoning_op_infer_multiplication",
+    "num_reasoning_op_infer_addition",
+    "num_reasoning_convert_min_sec",
+    "num_reasoning_convert_hour_min",
+    "num_reasoning_convert_day_hour",
+    "num_reasoning_convert_week_day",
+    "num_reasoning_convert_month_week",
+    "num_reasoning_convert_year_month",
+    "num_reasoning_convert_decade_year",
+    ]
 
-    df = pd.read_csv(f"results/csv/pythia-{size}-deduped/term_frquency_all_shots.csv")
-    df = df[df["task"].str.contains(task) & (df["checkpoint"] == 143000) & (df["fewshot"] == 16)]
+def jitter(values, j=0):
+    return values + np.random.normal(0, 0.05, values.shape)
 
-    for i in range(0, 100):
-        i = str(i)
-        x.append(freq[i])
-        y.append(df[df['task'].str.contains("_"+i)]['acc'].values[0])
-        z.append(size)
+model_list = [
+    "EleutherAI/pythia-13b-deduped",
+    "EleutherAI/pythia-6.7b-deduped",
+    "EleutherAI/pythia-2.7b-deduped",
+    "EleutherAI/pythia-1.3b-deduped",
+    "EleutherAI/pythia-800m-deduped",
+    # "EleutherAI/pythia-350m-deduped",
+    # "EleutherAI/pythia-125m-deduped",
+    # "EleutherAI/pythia-19m-deduped",
+]
 
-    data = pd.DataFrame(
-        data={
-            'x': x,
-            'y': y,
-            'z': z,
-            })
+sns.set_style("white")
+sns.set_context("poster")
 
-    ax = sns.regplot(
-        data=data, x="x", y="y",# hue="z",
-        fit_reg=True,
-        # ci=None,
-        label=size,
-        logx=True,
-        scatter=False
+task_names = [
+    "num_reasoning_arithmetic_multiplication",
+    "num_reasoning_arithmetic_addition",
+    "num_reasoning_op_infer_multiplication",
+    "num_reasoning_op_infer_addition",
+    ]
+
+for task in tqdm(task_names):
+
+    fig, axes = plt.subplots(1, len(model_list), figsize=(20, 7.5), tight_layout=True)
+
+    # for idx, model in enumerate(model_list):
+    for idx, model in enumerate(list(reversed(model_list))):
+
+        alphabet = string.ascii_lowercase[idx]
+        size = model.split("-")[1]
+        name = model.split("/")[-1]
+
+        n = 16
+
+        x, y, z, c = [], [], [], []
+        for checkpoint in show_checkpoints:
+
+            _freq = freq[checkpoint]
+            df = pd.read_csv(f"results/csv/pythia-{size}-deduped/term_frquency_all_shots.csv")
+            df = df[df["task"].str.contains(task) & (df["checkpoint"] == checkpoint) & (df["fewshot"] == n)]
+            # df = pd.read_csv(f"results/csv/{name}/term_frquency_all_shots.csv")
+            # df = df[df["task"].str.contains(task) & (df["fewshot"] == n)]
+
+            for i in range(0, 100):
+                count = str(i)
+                x.append(_freq[count])
+                y.append(df[df['task'].str.contains("_"+count)]['acc'].values[0])
+                z.append(str(checkpoint))
+
+        data = pd.DataFrame(
+            data={
+                'x': x,
+                'y': y,
+                'checkpoint': z,
+                })
+
+        bins = list(i*10**exp for exp in range(5, 10) for i in range(1, 10))
+        data['bin'] = pd.cut(data['x'], bins=bins, labels=bins[:-1]).astype(int)
+
+        sns.lineplot(
+            ax=axes[idx],
+            data=data,
+            x='bin',
+            y='y',
+            hue='checkpoint',
+            marker="o",
+            errorbar=None,
+            legend=False if idx != (len(model_list)-1) else True
         )
-    
-ax.set(xscale="log")
-ax.set_xlabel("Frequency")
-ax.set_ylabel("Avg. Accuracy")
-# ax.set_title("Multiplication Performance, 13b-deduped @143000 16-shot")
-ax.set_title("Multiplication Performance, All sizes @143000 16-shot")
-plt.legend(fontsize=10)
-plt.savefig('all_size-143000-arithmetic-freq-acc.png', dpi=200)
-plt.clf()
+
+        if idx == len(model_list)-1:
+            plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0)
+
+        if idx != 0:
+            axes[idx].tick_params(labelleft=False, left=False)
+
+        axes[idx].set(xscale="log")
+        axes[idx].set(ylim=(0.0, 1.0))
+        # axes[idx].set_xlabel(f"Frequency\n\n({alphabet}) {size}")
+        # axes[idx].set_ylabel("Avg. Accuracy")
+        axes[idx].set_xlabel(f"\n({alphabet}) {size}")
+        axes[idx].set_ylabel("")
+
+    figure_title = f"{task}_{n}-shot"
+    # plt.savefig('overleaf/{}.svg'.format(figure_title), dpi=200)
+    plt.savefig('overleaf/{}.pdf'.format(figure_title), dpi=200)
+    plt.savefig('overleaf/{}.png'.format(figure_title), dpi=200)
+    plt.clf()
