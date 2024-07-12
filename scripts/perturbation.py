274ab6ce4a5d279479d1103ce12b70cd5053871a
Sunny Duan <sunnyd@OPENMIND7.MIT.EDU>
2024-07-12 09:49:55 -0400

    Perturbation scripts

--- scripts/perturbation.py
+++ scripts/perturbation.py
@@ -1,5 +1,5 @@
 import os, sys
-sys.path.append('..')
+import click
 
 import matplotlib.pyplot as plt
 import torch
@@ -19,18 +19,20 @@ import polars as pl
 from transformers import GPTNeoXForCausalLM, AutoTokenizer
 import numpy as np
 
-NUM_PERTURB = 200
-BATCH_SIZE=256
-MODEL='1b-v0'
-CHECKPOINT=10000
 WORKER_ID = int(os.environ['WORKER_ID'])
-SUFFIX = os.environ.get('SUFFIX', '')
-INDICES = os.environ.get('INDICES', '')
-#all_indices = np.load(INDICES)
-output_fname = f'perturbation-{CHECKPOINT}-{WORKER_ID}{SUFFIX}.npz'
 
-def main():
-    df = pd.read_parquet('../results/analysis.parquet.gzip')  
+@click.command()
+@click.option('--input_dir', type=str, default="../results/mem_once")
+@click.option('--indices', type=str, default="../results/mem_once/indices.npy")
+@click.option('--output', type=str, default="../results/mem_once")
+@click.option('--num_perturb', type=int, default=200)
+@click.option('--model_id', type=str, default='1b-v0')
+@click.option('--checkpoint', type=int, default=10000)
+def main(input_dir, indices, output, num_perturb, model_id, checkpoint):
+    os.makedirs(f'{output}/perturb/', exist_ok=True)
+    output_fname = f'{output}/perturb/{checkpoint}_{WORKER_ID}'
+    df = pd.read_parquet(f'{input_dir}/analysis.parquet.gzip')  
+    indices = np.load(indices)
 
     # maxvals = df.groupby(df.index.get_level_values(0)).transform('max') 
     # indices_0_1 = np.array(list(set(df[(df["longest_match"] < 5 ) & (df.index.get_level_values(-1) == 10000)].index.get_level_values(0).to_numpy()).intersection(
@@ -44,40 +46,34 @@ def main():
 
     dataset = MMapIndexedDataset('/om/user/sunnyd/data/datasets--EleutherAI--pile-standard-pythia-preshuffled-merged/document', skip_warmup = True)
     tokenizer = AutoTokenizer.from_pretrained(
-    "EleutherAI/pythia-70m-deduped",
-    revision="step3000",
-    cache_dir="/om/user/sunnyd/transformers_cache",
+        "EleutherAI/pythia-70m-deduped",
+        revision="step3000",
+        cache_dir="/om/user/sunnyd/transformers_cache",
     )
 
     model = GPTNeoXForCausalLM.from_pretrained(
-        f"EleutherAI/pythia-{MODEL}",
+        f"EleutherAI/pythia-{model_id}",
         use_cache=False,
-        revision = f'step{CHECKPOINT}',
+        revision = f'step{checkpoint}',
         cache_dir=f"/om/user/sunnyd/transformers_cache/"
     ).half().eval().cuda()
 
-    # all_indices = np.concatenate([indices_0, indices_1])
-
-    all_indices = np.arange(21000 * 1024 + 1000 * WORKER_ID, 21000 * 1024 + 1000 * (WORKER_ID+1))
-    BATCH_SIZE=len(all_indices) // 16
-    # all_indices = all_indices[BATCH_SIZE * WORKER_ID: min(BATCH_SIZE * (WORKER_ID + 1), len(all_indices))]
-    # all_indices = np.arange(10000 * 1024 + 500 * WORKER_ID, 10000 * 1024 + 500 * (WORKER_ID+1))
-    # all_indices = np.arange(21000 * 1024 + 500 * WORKER_ID, 21000 * 1024 + 500 * (WORKER_ID+1))
-    data = np.array([dataset[idx.astype(np.int32).item()] for idx in all_indices])
+    batch_indices = np.split(indices, np.arange(0, len(indices), len(indices)//16))[WORKER_ID]
+    data = np.array([dataset[idx.astype(np.int32).item()] for idx in batch_indices])
     context_tokens = torch.tensor(data[:, :32].astype(np.int32)).to('cuda')
     with torch.no_grad():
-            gen_orig = model.generate(context_tokens.cuda(), temperature = 0.0, top_k = 0, top_p = 0, max_length = 96, min_length = 96)
+        gen_orig = model.generate(context_tokens.cuda(), temperature = 0.0, top_k = 0, top_p = 0, max_length = 96, min_length = 96)
 
     model2 = GPTNeoXForCausalLM.from_pretrained(
-        f"EleutherAI/pythia-{MODEL}",
+        f"EleutherAI/pythia-{model_id}",
         use_cache=False,
-        revision = f'step{CHECKPOINT}',
+        revision = f'step{checkpoint}',
         cache_dir=f"/om/user/sunnyd/transformers_cache/"
     ).half().eval().cuda()
 
     param1 = dict(model.named_parameters())
     gens = []
-    for i in tqdm(range(NUM_PERTURB)):
+    for i in tqdm(range(num_perturb)):
         with torch.no_grad():
             for k, p2 in model2.named_parameters():
                 p2.copy_(param1[k] + torch.randn_like(p2) * 2e-3)
@@ -87,4 +83,7 @@ def main():
 
     results = np.stack(gens)
 
-    np.savez(output_fname, results = results, indices = all_indices, gen_orig = gen_orig.detach().cpu().numpy(), data = data)
+    np.savez(output_fname, results = results, gen_orig = gen_orig.detach().cpu().numpy(), data = data, indices = batch_indices)
+
+if __name__ == "__main__":
+    main()
